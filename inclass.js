17e403c7dab0872929c1c0ee159797b5a0d32ac5
Marame Gattan <drqattan@hotmail.com>
2017-05-13 12:49:27 -0400

    updated

--- inclass.js
+++ inclass.js
@@ -9,8 +9,60 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
+
+// what reservations we have.
+// html welcome page
+// form page
+// table page
+// reservation
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "Sizzlin'.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+app.get("/form", function(req, res) {
+    res.sendFile(path.join(__dirname, "form.html"));
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/new", function(req, res) {
+    var newcharacter = req.body;
+    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newcharacter);
+
+    characters.push(newcharacter);
+
+    res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
